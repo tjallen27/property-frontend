323493fba96bbcea620bedff1b62f982fc34a7fc
tjallen27 <tjallen91@hotmail.co.uk>
2017-07-05 13:24:29 +0100

    Property API added to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 angular
   .module('blog', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate', 'ngMessages'])
-  .constant('API_URL', 'https://desolate-reaches-84582.herokuapp.com')
+  .constant('API_URL', 'https://thawing-scrubland-24337.herokuapp.com')
   .filter('startFrom', function(){
     return function(data, start){
       return data.slice(start);
